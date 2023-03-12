cc04511e01ce3666c318a50daacd4f45017f460f
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2023-03-12 13:36:31 +0530

    display options and take user input

--- Email Generator Lab 1 OOPs/src/com/ashutosh/main/Driver.java
+++ Email Generator Lab 1 OOPs/src/com/ashutosh/main/Driver.java
@@ -1,5 +1,7 @@
 package com.ashutosh.main;
 
+import java.util.Scanner;
+
 import com.ashutosh.model.Employee;
 import com.ashutosh.service.CredentialService;
 
@@ -8,6 +10,15 @@ public class Driver {
 	public static void main(String[] args) {
 		Employee empObj=new Employee("Ashutosh","Tripathi");
 		CredentialService csObj=new CredentialService();
+		int choice;
+		char[] password=csObj.generatePassword();
+		System.out.println("Please enter the department from the following");
+		System.out.println("1. Technical");
+		System.out.println("2. Admin");
+		System.out.println("3. Human Resource");
+		System.out.println("4. Legal");
+		Scanner sc=new Scanner(System.in);
+		choice=sc.nextInt();
 		
 	}
 
