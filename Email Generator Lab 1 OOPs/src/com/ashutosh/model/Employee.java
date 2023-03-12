296fee1ee3cc7f6f86300d1ea6e8a48118540d69
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2023-03-12 12:50:13 +0530

    create parameterized constructor

--- Email Generator Lab 1 OOPs/src/com/ashutosh/model/Employee.java
+++ Email Generator Lab 1 OOPs/src/com/ashutosh/model/Employee.java
@@ -2,5 +2,9 @@ package com.ashutosh.model;
 
 public class Employee {
 	private String firstName, lastName;
+	Employee(String firstName, String lastName) {
+		this.firstName=firstName;
+		this.lastName=lastName;
+	}
 	
 }
