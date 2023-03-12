483f8a2bc73efc997cb897c47afb78d78a29b6b9
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2023-03-12 13:53:19 +0530

    improve code readability

--- Email Generator Lab 1 OOPs/src/com/ashutosh/main/Driver.java
+++ Email Generator Lab 1 OOPs/src/com/ashutosh/main/Driver.java
@@ -10,6 +10,7 @@ public class Driver {
 	public static void main(String[] args) {
 		Employee empObj=new Employee("Ashutosh","Tripathi");
 		CredentialService csObj=new CredentialService();
+		Scanner sc=new Scanner(System.in);
 		int choice;
 		String email;
 		char[] password=csObj.generatePassword();
@@ -18,7 +19,6 @@ public class Driver {
 		System.out.println("2. Admin");
 		System.out.println("3. Human Resource");
 		System.out.println("4. Legal");
-		Scanner sc=new Scanner(System.in);
 		choice=sc.nextInt();
 		switch(choice) {
 		case 1:
