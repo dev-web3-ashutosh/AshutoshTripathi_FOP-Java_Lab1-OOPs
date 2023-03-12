4f371e32c02f5529f91c55a152fdfcff6868a9fb
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2023-03-12 13:28:35 +0530

    make constructor public

--- Email Generator Lab 1 OOPs/src/com/ashutosh/model/Employee.java
+++ Email Generator Lab 1 OOPs/src/com/ashutosh/model/Employee.java
@@ -2,7 +2,7 @@ package com.ashutosh.model;
 
 public class Employee {
 	private String firstName, lastName;
-	Employee(String firstName, String lastName) {
+	public Employee(String firstName, String lastName) {
 		this.firstName=firstName;
 		this.lastName=lastName;
 	}
