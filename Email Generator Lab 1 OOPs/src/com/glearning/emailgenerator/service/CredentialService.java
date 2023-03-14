c46ae84b8edf182b98a9ec5dfc85c049f0290542
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2023-03-14 21:51:02 +0530

    update generatePassword method

--- Email Generator Lab 1 OOPs/src/com/glearning/emailgenerator/service/CredentialService.java
+++ Email Generator Lab 1 OOPs/src/com/glearning/emailgenerator/service/CredentialService.java
@@ -12,8 +12,30 @@ public class CredentialService {
 		String numbers="0123456789";
 		String specialCharacters="!@#$%^&*()_=+-/?<>";
 		String values=capitalLetters+smallLetters+numbers+specialCharacters;
-		for(int i=0;i<8;i++) {
+		
+		//ensure pwd contains at least 1 Upper case letter
+		pwd[0]=capitalLetters.charAt(rand.nextInt(capitalLetters.length()));
+		
+		//ensure pwd contains at least 1 Lower case letter
+		pwd[1]=smallLetters.charAt(rand.nextInt(smallLetters.length()));
+		
+		//ensure pwd contains at least 1 number
+		pwd[2]=numbers.charAt(rand.nextInt(numbers.length()));
+		
+		//ensure pwd contains at least 1 special character
+		pwd[3]=specialCharacters.charAt(rand.nextInt(specialCharacters.length()));
+		
+		//fill remaining elements of char array pwd with random elements from string 'values'
+		for(int i=4;i<8;i++) {
 			pwd[i]=values.charAt(rand.nextInt(values.length()));
+		}
+		
+		//shuffle password
+		for(int i=0;i<8;i++) {
+			int j=rand.nextInt(8); //generates a random integer from 0 to 7
+			char temp=pwd[i];
+			pwd[i]=pwd[j];
+			pwd[j]=temp;
 		}
 		return pwd;
 	}
